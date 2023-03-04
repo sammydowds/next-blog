5377b5858388b8540edced2a1c790aa91a2558ed
Samuel Dowds <sammydowds1993@gmail.com>
2023-03-04 11:42:39 -0600

    fix: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
 				<title>About Me</title>
 				<meta name="description" content="About me page" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
 				<SingleColumnLayout>
--- pages/notes.tsx
+++ pages/notes.tsx
@@ -7,6 +7,7 @@ export default function Home() {
 				<title>Notes</title>
 				<meta name="description" content="Notes written" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
 				Notes here
