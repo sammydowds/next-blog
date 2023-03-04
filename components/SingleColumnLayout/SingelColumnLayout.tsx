7021216683a771aa5f8e9084c91afab96d8689e8
Samuel Dowds <sammydowds1993@gmail.com>
2023-03-04 10:57:09 -0600

    fix: creating first layout component

---
+++ components/SingleColumnLayout/SingelColumnLayout.tsx
@@ -0,0 +1,12 @@
+import { Box, VStack } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+export const SingleColumnLayout = ({ children }: { children: ReactNode | ReactNode[] }) => {
+  return (
+    <VStack padding="10px" alignItems="center">
+      <Box maxW="700px">
+        {children}
+      </Box>
+    </VStack>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
+import { SingleColumnLayout } from '@/components/SingleColumnLayout'
 
 export default function Home() {
 	return (
@@ -9,13 +11,144 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<main>
-				Hi, I am Sammy!
-				Github, Twitter, LinkedIn, and Resume
-				I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+				<SingleColumnLayout>
+					<Box>
+						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
 
-				Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
 
-				Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.
+						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.						Hi, I am Sammy!
+						Github, Twitter, LinkedIn, and Resume
+						I am a full-stack engineer who loves to learn and build new things. I started my career as a Mechanical Engineer after graduation from Oklahoma State University in 2016. My curiosity lead me to web dev (self-taught), and I have not looked back since.
+
+						Right now I am focused on frontend tech, but I also have an appreciation for the backend side of things (CRON, Django, Fastly, FastApi, etc). On the frontend, I have experience with Express, React, Redux, Node, Deno, and Next JS - typically all written in TypeScript.
+
+						Outside of appreciating the beautiful mess of Frontend, I enjoy gaming, running, and watching sports (big NBA guy). My focus is becoming a better engineer, staying healthy, and maintaining a healthy balance in life.
+					</Box>
+				</SingleColumnLayout>
 			</main>
 		</>
 	)
