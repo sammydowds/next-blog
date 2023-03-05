04ee36efab8189bfa3704d59eb5a6a998af0ab1f
Samuel Dowds <sammydowds1993@gmail.com>
2023-03-05 13:24:22 -0600

    feat: enable vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Header } from '../components/Header'
 import theme from '../theme'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
 			<Header />
 			<Component {...pageProps} />
+			<Analytics />
 		</ChakraProvider>
 	)
 }
