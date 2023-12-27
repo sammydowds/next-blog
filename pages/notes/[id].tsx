8d7ff34200fb5abedeccaeac2c5232768a39cea0
Samuel Dowds <sammydowds1993@gmail.com>
2023-12-27 12:49:06 -0600

    chore: update descriptions per note in head

--- pages/notes/[id].tsx
+++ pages/notes/[id].tsx
@@ -16,7 +16,7 @@ export default function NoteDetail({ note }: NoteDetailProps) {
     <>
       <Head>
         <title>{note.data.title}</title>
-        <meta name="description" content="Notes written" />
+        <meta name="description" content={note.data.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
