7a8bb2d8577300aacce57eaaa45af8ded6ee538f
Samuel Dowds <sammydowds1993@gmail.com>
2023-12-27 13:31:57 -0600

    chore: set www on base url sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://sd3.dev',
+  siteUrl: process.env.SITE_URL || 'https://www.sd3.dev',
   generateRobotsTxt: true
 }
