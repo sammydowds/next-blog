38446c6ec4d6f6f9814281bdb4996fd039c2239b
Samuel Dowds <sammydowds1993@gmail.com>
2024-01-14 09:14:22 -0600

    chore: add in eng blurb

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,7 @@ export default function Home() {
           <Box>
             <VStack align="start" gap="20px" mb="100px">
               <Heading>Hi, I am Sammy!</Heading>
+              <Heading size="md" as="h2">Welcome to my engineering blog.</Heading>
               <HighlightedPost />
               <Card>
                 <CardHeader p="16px" fontSize="14px" fontWeight="bold">
